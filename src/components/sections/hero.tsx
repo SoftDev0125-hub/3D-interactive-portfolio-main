24692b8cf807535c1d918704d96c177240cd864c
SoftDev0125-hub <antoni.kowalski940125@gmail.com>
2026-02-06 05:05:15 -0800

    change role and update resume

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -76,14 +76,14 @@ const HeroSection = () => {
                       "cursor-default font-display sm:text-xl md:text-xl whitespace-nowrap bg-clip-text "
                     )}
                   >
-                    AI Enthusiast
+                    Senior Full Stack Developer
                   </p>
                 </BlurIn>
               </div>
               <div className="mt-8 md:ml-2 flex flex-col gap-3">
                 <Link
                   href={
-                    "https://drive.google.com/file/d/1vjtWfqYGMIMK_rlBnmDEpKco9MiEaQss/view"
+                    "https://drive.google.com/file/d/1WdkzO_Z1U3M0fP5Q9KCHjAXcXN1gITsi/view?usp=sharing"
                   }
                   target="_blank"
                   className="flex-1"
