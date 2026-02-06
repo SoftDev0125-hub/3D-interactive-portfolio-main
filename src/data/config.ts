834849a1550a3635b089aa27806393c919549e54
SoftDev0125-hub <antoni.kowalski940125@gmail.com>
2026-02-06 04:43:26 -0800

    my portfolio first commit

---
+++ src/data/config.ts
@@ -0,0 +1,48 @@
+const config = {
+  title: "Abel Simbulan | Full Stack Developer & Creative Technologist",
+  description: {
+    long: "Explore the portfolio of Abel, a Computer Science Engineer with 1.5 year experience in Software Devlopment & Database management with hands-on experience in deploying & automating workflows, & collaborating with foreign clients. Strong analytical skills, effective communication, and a team-oriented approach to driving process improvements and delivering results.",
+    short:
+      "Discover the portfolio of Abel, a Full Stack Developer & Creative Technologist.",
+  },
+  keywords: [
+    "Abel",
+    "Abel Simbulan",
+    "abelsimbulan",
+    "abelsimbulan.com",
+    "Abel Simbulan portfolio",
+    "portfolio",
+    "DevOps",
+    "Devlopment",
+    "Database",
+    "AI Docker file optimizer",
+    "Finance me",
+    "creative technologist",
+    "web development",
+    "interactive websites",
+    // "GSAP",
+    // "React",
+    // "Next.js",
+    // "Spline",
+    // "Framer Motion",
+    "AWS",
+    "DevSecOps",
+    "AIOps",
+    "Azure",
+  ],
+  author: "Abel Simbulan",
+  email: "abelsimbulan1@outlook.com",
+  site: "https://abelsimbulan.com",
+
+  get ogImg() {
+    return this.site + "/assets/seo/og-image.png";
+  },
+  social: {
+    twitter: "https://x.com/AbelSimbul37521/",
+    linkedin: "https://www.linkedin.com/in/abel-simbulan-934a553a2/",
+    instagram: "https://www.instagram.com/abel0.916/",
+    facebook: "https://www.facebook.com/profile.php?id=100010029821759",
+    github: "https://github.com/SoftDev0125-hub/",
+  },
+};
+export { config };
